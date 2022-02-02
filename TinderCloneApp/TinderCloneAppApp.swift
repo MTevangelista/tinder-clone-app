3d0f855df7f87fbebc8aa9c085dc98698496b6c8
Matheus Evangelista <matheus.evangelista@g.globo>
2022-02-02 06:50:23 -0300

    Initial Commit

---
+++ TinderCloneApp/TinderCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneAppApp.swift
+//  TinderCloneApp
+//
+//  Created by matheus.evangelista on 02/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
